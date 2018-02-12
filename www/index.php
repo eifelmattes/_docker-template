802a0bf15aeecdfc85abe65b2037bea4f28d2c9d
mattes <matthias@behmann.org>
2018-02-12 19:03:58 +0100

    index.php hinzugefügt

--- www/index.php
+++ www/index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo 'Der Webserver läuft!'
+        echo 'Der Webserver läuft!';
         ?>
     </body>
 </html>
